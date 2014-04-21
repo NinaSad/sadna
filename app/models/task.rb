83a4d0a268339c542b9c2c43efe0d6c71b51d5db
Nina Goldenberg <ninasad88@gmail.com>
2014-04-21 21:19:53 +0300

    fixed assignee logic + added some styling to the main tasks page

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,8 @@
 class Task < ActiveRecord::Base
   belongs_to :user
 
+  belongs_to :doer, :class_name => "User" , :foreign_key => :assignee
+
   #scope :click, lambda{|query|
    # where(["project_id LIKE ?", "#{query}%"])
   #}
